0a32f4254494e20cc63c202a237c602b6a9087be
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-01-30 15:54:24 -0300

    feat: avg rating on books

--- src/repositories/books-repository.ts
+++ src/repositories/books-repository.ts
@@ -14,8 +14,22 @@ export async function findByTitle(title: string): Promise<BookEntity> {
   return data;
 }
 
-export async function findAll(): Promise<BookEntity[]> {
-  const data = await prisma.book.findMany();
+export type BookReviewed = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  rating: number;
+};
+
+export async function findAll(): Promise<BookReviewed[]> {
+  const data = prisma.$queryRaw<BookReviewed[]>`
+    select b.*,avg(r.rating)::float as "avgRating"
+    from books b
+    left join reviews r
+    on b.id = r.book_id
+    group by b.id;`;
+
   return data;
 }
 
